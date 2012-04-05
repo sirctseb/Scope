f725d29bcee6948727f30754194948f24c72fbe1
Christopher Best <bestchris@gmail.com>
2012-04-05 11:23:02 -0400

    added solution to test soar added include directory for dlls needed to talk to soar

---
+++ TestSoar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using sml;
+
+namespace TestSoar
+{
+	class Program
+	{
+		static bool Err(bool err, string errString)
+		{
+			if(err) {
+				Console.WriteLine(errString);
+				Console.ReadLine();
+			}
+			return err;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("starting");
+			// create a kernel
+			sml.Kernel kernel = sml.Kernel.CreateKernelInNewThread();
+
+			if (kernel.HadError())
+			{
+				Console.WriteLine("Error creating kernel");
+				return;
+			}
+
+			// create an agent
+			Agent agent = kernel.CreateAgent("test-agent");
+			if (kernel.HadError())
+			{
+				Console.WriteLine("Error creating agent");
+				return;
+			}
+
+			// load source
+			agent.LoadProductions("../../../../Scope/TestAgent/test-agent.soar");
+			if(Err(agent.HadError(), "Error loading source")) return;
+
+			// give input
+			Identifier input = agent.GetInputLink();
+			if(Err(agent.HadError(), "Error getting input link")) return;
+
+			// put stuff on input
+			input.CreateStringWME("text", "something");
+
+			agent.RunSelf(3);
+
+			// get output
+			for (int i = 0; i < agent.GetNumberCommands(); i++)
+			{
+				Identifier id = agent.GetCommand(i);
+				if (id.GetCommandName() == "response")
+				{
+					Console.WriteLine("response: " + id.GetParameterValue("text"));
+				}
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
