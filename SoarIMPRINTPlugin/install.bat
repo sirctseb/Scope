b29594b41ec01a4443edcd1151ebd8113d6eac06
Christopher Best <bestchris@gmail.com>
2012-09-17 15:22:03 -0400

    updated install.bat paths for umich computer

--- SoarIMPRINTPlugin/install.bat
+++ SoarIMPRINTPlugin/install.bat
@@ -1,4 +1,4 @@
-copy SoarIMPRINTPlugin\bin\debug\SoarIMPRINTPlugin.dll "C:\Users\christopher.j.best2\Desktop\IMPRINT Pro 3.5\"
-copy Utility\bin\debug\Utility.dll "C:\Users\christopher.j.best2\Desktop\IMPRINT Pro 3.5\"
-xcopy /E /Y ..\TestAgent\* "C:\Users\christopher.j.best2\Desktop\IMPRINT Pro 3.5\Scope\agent\"
-xcopy /E /Y ..\ScopeAgent\* "C:\Users\christopher.j.best2\Desktop\IMPRINT Pro 3.5\Scope\agent\"
+copy SoarIMPRINTPlugin\bin\debug\SoarIMPRINTPlugin.dll "C:\Program Files (x86)\IMPRINT Pro 3.5\"
+copy Utility\bin\debug\Utility.dll "C:\Program Files (x86)\IMPRINT Pro 3.5\"
+xcopy /E /Y ..\TestAgent\* "C:\Program Files (x86)\IMPRINT Pro 3.5\Scope\agent\"
+xcopy /E /Y ..\ScopeAgent\* "C:\Program Files (x86)\IMPRINT Pro 3.5\Scope\agent\"
